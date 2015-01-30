0d1d0dc8aa79f83d2a23359eaf668a2deacc723c
Chris Mountford <chris@atlassian.com>
2015-01-30 15:26:30 +1100

    bfx.js

--- bfx.js
+++ bfx.js
@@ -1,5 +1,5 @@
 var request = require('request-json')
 var client = request.newClient('https://api.bitfinex.com/v1/')
 client.get('pubticker/btcusd', function(err, res, body) {
-    return console.log("$" + body.last_price);
+    return console.log(body.last_price);
 });
